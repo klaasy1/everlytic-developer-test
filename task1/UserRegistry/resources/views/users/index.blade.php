f7e285855d29d8c662678ac440e40f1547f552f2
Klaas Rikhotso <klaas@360solution.co.za>
2023-04-17 10:17:27 +0200

    phpunit

--- task1/UserRegistry/resources/views/users/index.blade.php
+++ task1/UserRegistry/resources/views/users/index.blade.php
@@ -17,8 +17,8 @@
     <!--
     <link href="css/app.css" rel="stylesheet" />
     -->
-    <link href="css/styles.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="css/styles.css" rel="stylesheet" />
 </head>
 <body id="page-top">
 <!-- Masthead-->
